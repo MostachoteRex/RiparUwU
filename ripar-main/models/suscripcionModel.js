95fbf529683288f730f047f262ee8f965b7b96c0
Mostachote Rex <luisaj512@hotmail.com>
2024-09-05 17:18:04 -0500

    Cambio de funciones a clases y agregando su documentacion

--- ripar-main/models/suscripcionModel.js
+++ ripar-main/models/suscripcionModel.js
@@ -1,58 +1,101 @@
 import { SuscriptorEntity } from "./suscriptorModel.js"
 import {UsuarioDatosRestModel, UsuarioEntity} from "./usuarioModel.js"
 
-function SuscripcionCrearRequestModel(suscripcion){
-    this.noContrato = suscripcion.noContrato
-    this.idAsesor = suscripcion.idAsesor
-    this.idSuscriptor = suscripcion.idSuscriptor
-    this.fechaSuscripcion = suscripcion.fechaSuscripcion
-    this.valor = suscripcion.valor
-    this.metodoPago = suscripcion.metodoPago
+/**
+ * Modelo de solicitud para crear una suscripción.
+ * @class
+ * @param {Object} suscripcion - Objeto que contiene los datos de la suscripción.
+ */
+class SuscripcionCrearRequestModel {
+    constructor(suscripcion) {
+        this.noContrato = suscripcion.noContrato
+        this.idAsesor = suscripcion.idAsesor
+        this.idSuscriptor = suscripcion.idSuscriptor
+        this.fechaSuscripcion = suscripcion.fechaSuscripcion
+        this.valor = suscripcion.valor
+        this.metodoPago = suscripcion.metodoPago
+    }
 }
 
-function SuscripcionDatosRestModel(suscripcion){
-    this.idSuscripcion = suscripcion.idSuscripcion
-    this.noContrato = suscripcion.noContrato
-    this.fechaSuscripcion = suscripcion.fechaSuscripcion
-    this.fechaVencimiento = suscripcion.fechaVencimiento    
-    this.tipoSuscripcion = suscripcion.tipoSuscripcion
-    this.estado = suscripcion.estado
-    this.fechaRegistro = suscripcion.fechaRegistro
-    this.suscriptorEntity = new SuscriptorEntity(suscripcion.suscriptorEntity)
-    this.asesorEntity = new UsuarioEntity(suscripcion.asesorEntity)  
-    this.valor = suscripcion.valor    
-    /* this.pacienteEntity = suscripcion.pacienteEntity.map(paciente => new PacienteEntity(paciente)); */
-    /* this.usuarioEntity= new UsuarioDatosRestModel(especialidad.usuarioEntity) */
+/**
+ * Modelo de datos REST para una suscripción.
+ * @class
+ * @param {Object} suscripcion - Objeto que contiene los datos de la suscripción.
+ */
+class SuscripcionDatosRestModel {
+    constructor(suscripcion) {
+        this.idSuscripcion = suscripcion.idSuscripcion
+        this.noContrato = suscripcion.noContrato
+        this.fechaSuscripcion = suscripcion.fechaSuscripcion
+        this.fechaVencimiento = suscripcion.fechaVencimiento    
+        this.tipoSuscripcion = suscripcion.tipoSuscripcion
+        this.estado = suscripcion.estado
+        this.fechaRegistro = suscripcion.fechaRegistro
+        this.suscriptorEntity = new SuscriptorEntity(suscripcion.suscriptorEntity)
+        this.asesorEntity = new UsuarioEntity(suscripcion.asesorEntity)  
+        this.valor = suscripcion.valor
+        this.pacienteEntity = suscripcion.pacienteEntity?.map(paciente => new PacienteEntity(paciente)) || []
+        this.usuarioEntity = suscripcion.usuarioEntity ? new UsuarioDatosRestModel(suscripcion.usuarioEntity) : null
+    }
 }
 
-function SuscripcionActualizarReqModel(suscripcion){
-    this.fechaSuscripcion = suscripcion.fechaSuscripcion
-    this.idAsesor = suscripcion.idAsesor
-    this.tipoSuscripcion = suscripcion.tipoSuscripcion
+/**
+ * Modelo de solicitud para actualizar una suscripción.
+ * @class
+ * @param {Object} suscripcion - Objeto que contiene los datos de la suscripción.
+ */
+class SuscripcionActualizarReqModel {
+    constructor(suscripcion) {
+        this.fechaSuscripcion = suscripcion.fechaSuscripcion
+        this.idAsesor = suscripcion.idAsesor
+        this.tipoSuscripcion = suscripcion.tipoSuscripcion
+    }
 }
 
-function SuscripcionEntity(suscripcion){
-    this.idSuscripcion = suscripcion.idSuscripcion
-    this.noContrato = suscripcion.noContrato
-    this.fechaSuscripcion = suscripcion.fechaSuscripcion
-    this.fechaVencimiento = suscripcion.fechaVencimiento
-    this.suscriptorEntity = new SuscriptorEntity(suscripcion.suscriptorEntity)
+/**
+ * Entidad de una suscripción.
+ * @class
+ * @param {Object} suscripcion - Objeto que contiene los datos de la suscripción.
+ */
+class SuscripcionEntity {
+    constructor(suscripcion) {
+        this.idSuscripcion = suscripcion.idSuscripcion
+        this.noContrato = suscripcion.noContrato
+        this.fechaSuscripcion = suscripcion.fechaSuscripcion
+        this.fechaVencimiento = suscripcion.fechaVencimiento
+        this.suscriptorEntity = new SuscriptorEntity(suscripcion.suscriptorEntity)
+
+    }
 }
 
-function SuscripcionDatosResModel(suscripcion){
-    this.idSuscripcion = suscripcion.idSuscripcion
-    this.noContrato = suscripcion.noContrato            
-    this.estado = suscripcion.estado        
-    this.pacienteEntity = suscripcion.pacienteEntity.map(paciente => new PacienteEntity(paciente));
-    /* this.usuarioEntity= new UsuarioDatosRestModel(especialidad.usuarioEntity) */
+/**
+ * Modelo de respuesta de datos para una suscripción.
+ * @class
+ * @param {Object} suscripcion - Objeto que contiene los datos de la suscripción.
+ */
+class SuscripcionDatosResModel {
+    constructor(suscripcion) {
+        this.idSuscripcion = suscripcion.idSuscripcion
+        this.noContrato = suscripcion.noContrato            
+        this.estado = suscripcion.estado        
+        this.pacienteEntity = suscripcion.pacienteEntity.map(paciente => new PacienteEntity(paciente)) || []
+        this.usuarioEntity = suscripcion.usuarioEntity ? new UsuarioDatosRestModel(suscripcion.usuarioEntity) : null
+    }
 }
 
-function PacienteEntity(suscripcion){
-    this.idBeneficiario = suscripcion.idBeneficiario
-    this.nombre = suscripcion.nombre
-    this.primerApellido = suscripcion.primerApellido
-    this.segundoApellido = suscripcion.segundoApellido
-    this.documento = suscripcion.documento
+/**
+ * Entidad de un paciente.
+ * @class
+ * @param {Object} suscripcion - Objeto que contiene los datos del paciente.
+ */
+class PacienteEntity {
+    constructor(suscripcion) {
+        this.idBeneficiario = suscripcion.idBeneficiario
+        this.nombre = suscripcion.nombre
+        this.primerApellido = suscripcion.primerApellido
+        this.segundoApellido = suscripcion.segundoApellido
+        this.documento = suscripcion.documento
+    }
 }
 
-export { SuscripcionCrearRequestModel, SuscripcionDatosRestModel, SuscripcionActualizarReqModel, SuscripcionEntity, PacienteEntity, SuscripcionDatosResModel }
+export { SuscripcionCrearRequestModel, SuscripcionDatosRestModel, SuscripcionActualizarReqModel, SuscripcionEntity, SuscripcionDatosResModel, PacienteEntity }
